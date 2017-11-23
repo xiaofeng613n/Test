d3dd0aba87480633144f823d12500bc6ef5cd884
xiaofeng613n <2289472333@qq.com>
2017-11-23 22:11:15 +0800

    1123home

---
+++ src/main/java/com/xiaofeng/jvm/CommandTest.java
@@ -0,0 +1,15 @@
+package com.xiaofeng.jvm;
+
+import sun.tools.jps.Jps;
+
+/**
+ * Created by xiao on 2017/11/23.
+ */
+public class CommandTest {
+
+	public static void main(String[] args)
+	{
+		String[] params = new String[]{};
+		Jps.main(params);
+ 	}
+}
