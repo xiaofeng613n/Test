677f0b5d79ad5e971213611fb707c0340c0b4022
xiaofeng613n <2289472333@qq.com>
2017-12-27 00:07:47 +0800

    tkmybatis

---
+++ src/main/java/com/xiaofeng/mybatis/entity/CustomerEntity.java
@@ -0,0 +1,93 @@
+package com.xiaofeng.mybatis.entity;
+
+import javax.persistence.*;
+
+@Table(name = "customer")
+public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 信息
+     */
+    private String info;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取姓名
+     *
+     * @return name - 姓名
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置姓名
+     *
+     * @param name 姓名
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取信息
+     *
+     * @return info - 信息
+     */
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * 设置信息
+     *
+     * @param info 信息
+     */
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
