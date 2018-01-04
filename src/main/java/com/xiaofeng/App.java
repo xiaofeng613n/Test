4f5c307ee48432e7a27f398e97bbeac3726be7c8
xiaofeng613n <2289472333@qq.com>
2018-01-04 22:13:36 +0800

    0104

--- src/main/java/com/xiaofeng/App.java
+++ src/main/java/com/xiaofeng/App.java
@@ -1,10 +1,14 @@
 package com.xiaofeng;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,10 +29,17 @@ public class App
 //         ExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
 
-        String s = "{\"query\": \"FROM \\\"M\\\" FROM \\\"jvm-db\\\".\\\"autogen\\\".\\\"%s\\\" WHERE time > :dashboardTime:\",}%";
-      String s1 = s.replaceAll("FROM","xx");
-        System.out.println(s);
-        System.out.println(s1);
+//        String s = "{\"query\": \"FROM \\\"M\\\" FROM \\\"jvm-db\\\".\\\"autogen\\\".\\\"%s\\\" WHERE time > :dashboardTime:\",}%";
+//      String s1 = s.replaceAll("FROM","xx");
+//        System.out.println(s);
+//        System.out.println(s1);
+        Consumer<String> consumer = string -> System.out.println(string);
+        for (int i = 0; i < 4; i ++){
+            consumer.accept("aa");
+        }
+        //Predicate<Map<String,Object>> filter =  stringObjectMap -> stringObjectMap.forEach( c -> );
+        Map<String,Object> map = null;
+        List<Object> list = map.values();
     }
 
     public static void test()
